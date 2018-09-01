6905220ae8025b1509519bd2e9a4eb19395822ee
Collins <collinsdelan@gmail.com>
2018-09-01 12:14:52 +0300

    Add fetch engineers func in admin controller

--- controllers/adminController.go
+++ controllers/adminController.go
@@ -1,1 +1,15 @@
 package controllers
+
+import (
+	"daas/models"
+	u "daas/utils"
+	"net/http"
+)
+
+var GetAllEngineers = func(w http.ResponseWriter, r *http.Request) {
+
+	data := models.GetEngineers()
+	resp := u.Message(true, "success")
+	resp["data"] = data
+	u.Respond(w, resp)
+}
