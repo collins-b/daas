d822118e0b69b90e478fe841a5df50160a272fe3
Collins <collinsdelan@gmail.com>
2018-09-01 18:16:51 +0300

    Add get specific engineer by emaill addr

--- controllers/adminController.go
+++ controllers/adminController.go
@@ -3,7 +3,10 @@ package controllers
 import (
 	"daas/models"
 	u "daas/utils"
+	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 var GetAllEngineers = func(w http.ResponseWriter, r *http.Request) {
@@ -12,4 +15,25 @@ var GetAllEngineers = func(w http.ResponseWriter, r *http.Request) {
 	resp := u.Message(true, "success")
 	resp["data"] = data
 	u.Respond(w, resp)
+}
+
+var GetEngineer = func(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	id := params["id"]
+	fmt.Println(id)
+	data := models.GetEngineer(id)
+	if data == nil {
+		errorHandler(w, r, http.StatusNotFound)
+		return
+	}
+	resp := u.Message(true, "success")
+	resp["data"] = data
+	u.Respond(w, resp)
+}
+
+func errorHandler(w http.ResponseWriter, r *http.Request, status int) {
+	w.WriteHeader(status)
+	if status == http.StatusNotFound {
+		fmt.Fprint(w, "An engineer with that email address doesn't exist.")
+	}
 }
